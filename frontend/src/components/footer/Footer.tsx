959f648646a7c5cf0036fe17a5a707c540d39ec5
JEC07 <jec.dev.22@gmail.com>
2022-07-02 20:07:16 -0300

    Update footer

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import '../../styles/footer.css'
+
+const Footer = () => {
+  return <footer className='footer'>Esto es el Footer</footer>
+}
+
+export default Footer
