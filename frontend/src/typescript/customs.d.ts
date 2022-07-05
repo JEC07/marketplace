64883d04dc1352217dcdf5d8b28cd29dcc920557
unknown <jec.dev.22@gmail.com>
2022-07-05 20:19:55 -0300

    Add declarations for .png .jpg .jpeg .gif
    Necesario en produccion para importar las imagenes en TypeScript.

--- frontend/src/typescript/customs.d.ts
+++ frontend/src/typescript/customs.d.ts
@@ -2,3 +2,23 @@ declare module '*.svg' {
   const content: any
   export default content
 }
+
+declare module '*.png' {
+  const value: any
+  export default value
+}
+
+declare module '*.gif' {
+  const value: any
+  export default value
+}
+
+declare module '*.jpg' {
+  const value: any
+  export default value
+}
+
+declare module '*.jpeg' {
+  const value: any
+  export default value
+}
