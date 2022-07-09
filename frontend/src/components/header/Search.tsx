7a59c6a39032b9675aed74947160e405d5bc154e
unknown <jec.dev.22@gmail.com>
2022-07-09 01:11:24 -0300

    Agregar hook de react-router
    *Cambios en la funciones.
    
    *Cambio en el retorno del componente.
    
    *Cambio en el diseño.

--- frontend/src/components/header/Search.tsx
+++ frontend/src/components/header/Search.tsx
@@ -1,32 +1,42 @@
 import * as React from 'react'
-import { useState, ChangeEvent, KeyboardEvent } from 'react'
-import { getPokemonByName } from '../../services/pokemonApi'
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import '../../styles/search.css'
+import searchIcon from '../../assets/search.png'
 
 const Search: React.FC = () => {
   const [pokemonName, setPokemonName] = useState<string>('')
+  const [activeForm, setActiveForm] = useState<boolean>(false)
+  const navigate = useNavigate()
 
   const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setPokemonName(e.target.value.toLowerCase().trim())
+    const value = e.target.value.toLowerCase().trim()
+    setPokemonName(value)
+
+    if (value) return setActiveForm(true)
+    setActiveForm(false)
   }
 
-  const handleKeyUpSearch = async (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter' && pokemonName) {
-      const data = await getPokemonByName(pokemonName)
-      console.log(data)
-    }
+  const handleSubmiSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (pokemonName) navigate(`search/${pokemonName}`, { replace: true })
   }
 
   return (
-    <div className='search'>
-      <input className='search__input' type="text"
+    <form className={activeForm ? 'search-form search-form--active' : 'search-form'} onSubmit={handleSubmiSearch}>
+      <input
+        className='search-form__input'
+        type='text'
         value={pokemonName}
         onChange={handleChangeSearch}
-        onKeyUp={handleKeyUpSearch}
       />
-      <img className='search__icon' src="icon-search" alt="icon-search" />
-    </div>
+
+      <button className='search-form__btn' type='submit'>
+        <img className='search-form__btn-icon' src={searchIcon} alt="search-icon" />
+      </button>
+    </form>
   )
 }
 
